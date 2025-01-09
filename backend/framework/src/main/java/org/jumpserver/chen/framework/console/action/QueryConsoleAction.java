9c56bfe880ea2029bf5d7b81efd5c106adc8135d
Aaron3S <chenyang@fit2cloud.com>
2025-01-09 18:40:50 +0800

    feat:  优化 sql 执行大小

--- backend/framework/src/main/java/org/jumpserver/chen/framework/console/action/QueryConsoleAction.java
+++ backend/framework/src/main/java/org/jumpserver/chen/framework/console/action/QueryConsoleAction.java
@@ -7,6 +7,8 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class QueryConsoleAction extends Action {
     public static final String ACTION_RUN_SQL = "run_sql";
+    public static final String ACTION_RUN_SQL_CHUNK = "run_sql_chunk";
+    public static final String ACTION_RUN_SQL_COMPLETE = "run_sql_complete";
     public static final String ACTION_RUN_SQL_FILE = "run_sql_file";
     public static final String ACTION_CANCEL = "cancel";
     public static final String ACTION_CHANGE_CURRENT_CONTEXT = "change_current_context";
