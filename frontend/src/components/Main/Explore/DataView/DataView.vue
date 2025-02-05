c06944b652798165322905500b0e1aa579e803d8
jiangweidong <1053570670@qq.com>
2025-02-05 09:52:41 +0800

    feat: Support right-click to copy SQL

--- frontend/src/components/Main/Explore/DataView/DataView.vue
+++ frontend/src/components/Main/Explore/DataView/DataView.vue
@@ -1,7 +1,8 @@
 <template>
-  <div v-loading="state.loading" class="data-view" id="doc">
-    <ExportDataDialog :visible.sync="exportDataDialogVisible" @submit="onExportSubmit"/>
-    <Toolbar :items="iToolBarItems"/>
+  <div id="doc" v-loading="state.loading" class="data-view" @contextmenu.prevent="preventDefaultContextMenu">
+    <RightMenu ref="rightMenu" :menus="menus" />
+    <ExportDataDialog :visible.sync="exportDataDialogVisible" @submit="onExportSubmit" />
+    <Toolbar :items="iToolBarItems" />
     <AgGridVue
       :rowData="rowData"
       :columnDefs="colDefs"
@@ -9,22 +10,26 @@
       style="height: 100%"
       :defaultColDef="defaultColDef"
       :gridOptions="gridOptions"
+      @cell-context-menu="showContextMenu"
     />
   </div>
 </template>
 
 <script>
+import store from '@/store'
 import Toolbar from '@/framework/components/Toolbar/index.vue'
 import { Subject } from 'rxjs'
 import ExportDataDialog from '@/components/Main/Explore/DataView/ExportDataDialog.vue'
 import { AgGridVue } from 'ag-grid-vue'
+import RightMenu from '@/components/Main/Explore/DataView/RightMenu.vue'
+import { SpecialCharacters, GeneralInsertSQL, GeneralUpdateSQL } from './const'
 
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-balham.css'
 
 export default {
   name: 'DataView',
-  components: { ExportDataDialog, Toolbar, AgGridVue },
+  components: { ExportDataDialog, Toolbar, AgGridVue, RightMenu },
   props: {
     meta: {
       type: Object,
@@ -164,7 +169,30 @@ export default {
         suppressMovableColumnsHints: true,
         suppressSortingHints: true
       },
-      init: false
+      init: false,
+      currentRow: null,
+      menus: [
+        {
+          name: 'copy',
+          title: this.$t('Copy'),
+          icon: 'el-icon-document-copy',
+          hidden: () => { return !store.getters.profile.canCopy },
+          children: [
+            {
+              name: 'copy-insert',
+              title: this.$t('InsertStatement'),
+              icon: 'el-icon-document-copy',
+              callback: () => this.handleCopy('insert')
+            },
+            {
+              name: 'copy-update',
+              title: this.$t('UpdateStatement'),
+              icon: 'el-icon-document-copy',
+              callback: () => this.handleCopy('update')
+            }
+          ]
+        }
+      ]
     }
   },
   computed: {
@@ -231,9 +259,64 @@ export default {
     onExportSubmit(scope) {
       this.exportDataDialogVisible = false
       this.$emit('action', { action: 'export', data: scope })
+    },
+    showContextMenu(params) {
+      this.currentRow = params.data
+      this.$refs.rightMenu.show(params.event)
+    },
+    preventDefaultContextMenu(event) {
+      event.preventDefault()
+    },
+    wrap(str, specChar) {
+      const result = str ? str.trim() : ''
+      return `${specChar}${result}${specChar}`
+    },
+    handleCopy(action) {
+      let sql = ''
+      let fields = ''
+      let values = ''
+      let updated_attrs = ''
+      let conditional_attrs = ''
+      let hasPrimary = false
+      const dbType = store.getters.profile.dbType
+      const { schema, table } = this.meta
+      const char = SpecialCharacters[dbType]
+      const tableName = `${this.wrap(schema, char)}.${this.wrap(table, char)}`
+      const primaryKeys = ['id']
+      for (let i = 0; i < this.colDefs.length; i++) {
+        const fieldName = this.colDefs[i].field
+        const fieldValue = `'${(this.currentRow[fieldName] || '')}'`
+        if (action === 'insert') {
+          fields += (i > 0 ? ', ' : '') + this.wrap(fieldName, char)
+          values += (i > 0 ? ', ' : '') + `${fieldValue}`
+          sql = GeneralInsertSQL
+            .replace('{table_name}', tableName)
+            .replace('{fields}', fields)
+            .replace('{values}', values)
+        } else {
+          if (primaryKeys.includes(fieldName)) {
+            hasPrimary = true
+          } else {
+            updated_attrs += (i > 0 ? ', ' : '') + `${this.wrap(fieldName, char)} = ${fieldValue}`
+          }
+          if (hasPrimary) {
+            conditional_attrs = `${this.wrap('id', char)} = '${this.currentRow['id']}'`
+          } else {
+            conditional_attrs = `${updated_attrs} LIMIT 1`
+          }
+          sql = GeneralUpdateSQL
+            .replace('{table_name}', tableName)
+            .replace('{updated_attrs}', updated_attrs)
+            .replace('{conditional_attrs}', conditional_attrs)
+        }
+      }
+      navigator.clipboard.writeText(sql).then(() => {
+        this.$message.success(this.$t('CopySucceeded'))
+      }).catch((error) => {
+        this.$message.error(`${this.$t('CopyFailed')}: ${error}`)
+      })
     }
   }
-
 }
 </script>
 
